037a64c5f337a01a1107c444a69e9b18d1b237e7
owpeter <pt909795@163.com>
2024-04-11 19:47:44 +0800

    new function: dfs

--- hello.c
+++ hello.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+void dfs();
+
 int main(){
     printf("hello world\n");
     printf("nihao\n");
     return 0;
+}
+
+void dfs(){
+    int a;
+    scanf("%d",&a);
+    printf("%d",a);
 }
